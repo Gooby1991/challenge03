c332412369e9bce1be701576ce170bb3928bfa6e
Gooby1991 <gabriel.alves1991@gmail.com>
2020-09-03 15:35:34 -0300

    added input and search

--- src/App.js
+++ src/App.js
@@ -1,29 +1,115 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState("");
+  const [URL, setURL] = useState("");
+  const [techs, setTechs] = useState("");
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    api.get("/repositories").then((response) => setRepositories(response.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = {
+      title: `${title}`,
+      url: `${URL}`,
+      techs: `${techs}`,
+    };
+
+    const response = await api.post("/repositories", repository);
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    let provisory = repositories;
+
+    const indexFound = provisory.findIndex(
+      (repository) => repository.id === id
+    );
+
+    provisory.splice(indexFound, 1);
+
+    console.log(provisory);
+
+    setRepositories([...provisory]);
+  }
+
+  function handleInputTitle(key) {
+    setTitle(key);
+  }
+
+  function handleInputURL(key) {
+    setURL(key);
+  }
+
+  function handleInputTechs(key) {
+    setTechs(key);
+  }
+
+  function handleInputSearch(key) {
+    setSearch(key);
+  }
+
+  async function handleSearch(e) {
+    e.preventDefault();
+    await api
+      .get(`/repositories?query=${search}`)
+      .then((response) => setRepositories(response.data));
   }
 
   return (
     <div>
+      <form>
+        <label>
+          Search
+          <input
+            value={search}
+            onChange={(e) => handleInputSearch(e.target.value)}
+          />
+        </label>
+        <button onClick={(e) => handleSearch(e)}>Buscar</button>
+      </form>
+
+      <form>
+        <label>
+          Title
+          <input
+            value={title}
+            onChange={(e) => handleInputTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          URL
+          <input value={URL} onChange={(e) => handleInputURL(e.target.value)} />
+        </label>
+        <label>
+          Techs
+          <input
+            value={techs}
+            onChange={(e) => handleInputTechs(e.target.value)}
+          />
+        </label>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </form>
+
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
